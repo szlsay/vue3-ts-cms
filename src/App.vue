9414d3d56f4450619dceebc7110d254eb87b048c
szlsay <409178030@qq.com>
2021-11-26 15:58:15 +0800

    feat(02): router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
-
-console.log('test commit')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
